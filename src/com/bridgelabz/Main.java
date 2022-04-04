2594660d9f05866ab41428ee2f3a03acf3c88a78
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 <Vivekpatil254@gmail.com>
2022-04-04 14:52:41 +0530

    First Commit

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,53 @@
+package com.bridgelabz;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number between 1 to 12");
+        Scanner scanner = new Scanner(System.in);
+        int monthNumber = scanner.nextInt();
+
+        switch (monthNumber) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("september");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+        }
+
+    }
+}
